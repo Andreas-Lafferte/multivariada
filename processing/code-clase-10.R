6e312c69035428dc2de753611d8263df919f99a6
Andreas-Lafferte <andreas.lafferte.tamayo@gmail.com>
2021-05-06 02:11:19 -0400

    code classes

--- processing/code-clase-10.R
+++ processing/code-clase-10.R
@@ -52,5 +52,139 @@ m01 <- glm(voto~sexo,data = enacoes,family = "binomial")
 m02 <- glm(voto~edad,data = enacoes,family = "binomial")
 m03 <- glm(voto~sexo+edad+educ,data = enacoes,family = "binomial")
 
-sjPlot::tab_model(list(m00, m01, m02, m03), show.ci=FALSE, p.style = "stars", dv.labels = c("Modelo 0", "Modelo 1", "Modelo 2", "Modelo 3"),string.pred = "Predictores", string.est = "β")
+screenreg(l = list(m00, m01, m02, m03), doctype = FALSE,
+          custom.coef.names=c("Intercepto","Sexo (Mujer=1)","Edad","Primaria","Secundaria", "Técnica o Superior","Universitaria o postgrado"),
+          custom.model.names = c("Modelo 0","Modelo 1","Modelo 2", "Modelo 3"))
 
+# 5.1. Modelo nulo ----
+
+## El modelo sin predictores nos permite conocer la probabilidad de votar. En este caso, vemos que se 
+## obtiene un intercepto de 0.71 correspondiente a [log(p/1-p)]. En este caso es la probabilidad de que 
+## una persona participe en las elecciones como votante (voto = 1)
+
+## En otras palabras el intercepto del modelo sin predictores nos entrega la log-odds de votar en las elecciones. 
+## Adicionalmente, podemos transformar nuevamente a unidades de probabilidad de la siguiente manera: 
+
+p = exp(0.71)/(1 +  exp(0.71))
+p
+
+# 5.2. Modelo con predictor binario ----
+
+## Los resultados muestran el valor del intercepto (0.77) y para sexo (-0.10).
+## ¿cuales son las chances (odds) de votar para los hombres y para las mujeres? 
+
+(549/803)/(254/803) # hombres
+(776/1171)/(395/1171) # mujeres
+
+(776/395)/(549/254) # odds ratio mujeres sobre hombres
+##  Las odds de votar en las elecciones son son 9,5% más bajas para las mujeres respecto a los hombres.
+
+## Usando las odds de votar para los hombres calculada en el apartado anterior, 
+## podemos confirmarlo: log(2.16) = 0.77. A su vez, el log-odds para las mujeres corresponde al 
+## log del odds-ratio de las odds de las mujeres y de los hombres calculado 
+## anteriormente: log(0.907) = -0.10. El output tradicional de R cuando usamos glm() nos entrega los 
+## coeficientes en unidades de log-odds, por tanto es necesario realizar la exponenciación de dichos 
+## coeficientes para realizar la interpretación en unidades de odds-ratio.
+
+# 5.3. Modelo con predictor continuo ----
+
+## La ecuacion de regresion para la variable edad es: logit(p) = -1.07 + 0.04 * edad. Revisar sito web.
+
+## log odds: por una unidad de incremento en edad (1 año), el cambio esperado en los log-odds es de 0.04.
+
+## odds: por el incremento de una unidad en edad (1 año), se espera ver un incremento aproximado de un 4% en las odds de ir a votar}.
+
+# 5.4. Modelo multivariado ----
+
+## Cada coeficiente prepresenta el cambio predicho en el log-odds de votar por un incremento/cambio en la 
+## variable, manteniendo todas las demás variables constantes. Cada coeficiente exponenciado (exp), 
+## representa el ratio de dos odds (por ejemplo, mujer respecto a hombre), o el cambio en las odds por el 
+## incremento de una unidad de la variable independiente (por ejemplo, un año de edad), manteniendo las demás
+## variables constantes.
+
+## Odds ratio mujeres sobre hombres = -0.02 -> las odds de votar en las elecciones son 1,9% mas bajas para las mujeres que los hombres, ceteris paribus.
+## Repetir
+
+# 5.4.1. Probabilidades predichas ----
+m03$coefficients
+
+# calculamos log-odds
+# Intercept     sexo*1    edad*55    educ2       educ3      educ4      educ5
+ed01<- -2.03578412+(-0.0235*1)+(0.0491*55)+(0.3365*0)+(0.4691*0)+(0.8844*0)+(1.5260*0) # mujer 55 anios Primaria incompleta
+ed02<- -2.03578412+(-0.0235*1)+(0.0491*55)+(0.3365*1)+(0.4691*0)+(0.8844*0)+(1.5260*0) # mujer 55 anios Primaria
+ed03<- -2.03578412+(-0.0235*1)+(0.0491*55)+(0.3365*0)+(0.4691*1)+(0.8844*0)+(1.5260*0) # mujer 55 anios Secundaria
+ed04<- -2.03578412+(-0.0235*1)+(0.0491*55)+(0.3365*0)+(0.4691*0)+(0.8844*1)+(1.5260*0) # mujer 55 anios Tecnica
+ed05<- -2.03578412+(-0.0235*1)+(0.0491*55)+(0.3365*0)+(0.4691*0)+(0.8844*0)+(1.5260*1) # mujer 55 anios Universitaria
+
+# calculamos prob predichas
+pr.ed01<- exp(ed01)/(1+exp(ed01))
+pr.ed02<- exp(ed02)/(1+exp(ed02))
+pr.ed03<- exp(ed03)/(1+exp(ed03))
+pr.ed04<- exp(ed04)/(1+exp(ed04))
+pr.ed05<- exp(ed05)/(1+exp(ed05))
+
+plot_model(m03,type = "pred",
+           terms = "educ",
+           title = "Probabilidades predichas para Voto según nivel educacional") + geom_line()
+
+# 6. Medidas de ajuste ----
+
+# 6.1. Devianza ----
+
+## Comparar las verosimilitudes del modelo con predictores, respecto a un modelo con menos predictores
+test01<- anova(m00,m01,test = "Chisq")
+test02<- anova(m00,m02,test = "Chisq")
+test03<- anova(m00,m03,test = "Chisq")
+lrt01<- rbind(test01,test02,test03) %>% unique()
+row.names(lrt01) <- c("Modelo nulo",
+                      "Modelo 1",
+                      "Modelo 2",
+                      "Modelo 3")
+knitr::kable(lrt01,digits = 3, caption = "Test de devianza entre modelos")
+
+# guardar valores de p
+test.pvalues1<- test01$`Pr(>Chi)`[2]
+test.pvalues2<- test02$`Pr(>Chi)`[2]
+test.pvalues3<- test03$`Pr(>Chi)`[2]
+
+# 6.2. Pseudo R2 McFadden ---- 
+
+## Basada en los likelihood, nos entrega una magnitud comparativa entre el modelo con predictores y el modelo nulo
+1-(logLik(m01)[1]/ logLik(m00)[1]) # modelo 1 vs modelo nulo
+1-(logLik(m02)[1]/ logLik(m00)[1]) # modelo 2 vs modelo nulo
+1-(logLik(m03)[1]/ logLik(m00)[1]) # modelo 3 vs modelo nulo
+
+# Con DescTools
+mfr2.00 <- DescTools::PseudoR2(m00)
+mfr2.01 <- DescTools::PseudoR2(m01)
+mfr2.02 <- DescTools::PseudoR2(m02)
+mfr2.03 <- DescTools::PseudoR2(m03)
+
+r2<- as.data.frame(cbind(c(mfr2.00,mfr2.01,mfr2.02,mfr2.03)))
+rownames(r2) <- c("Modelo nulo",
+                  "Modelo 1",
+                  "Modelo 2",
+                  "Modelo 3")
+knitr::kable(r2,digits = 3, col.names = c("McFadden R2"))
+
+# 7. BONUS TRACK: presentacion resultados -----
+screenreg(l = list(m03,m03))
+
+or <- texreg::extract(m03)
+or@coef <- exp(or@coef)
+
+screenreg(l = list(m03,or), doctype = F,caption = "",caption.above = T,
+        custom.model.names = c("Modelo 3", "Modelo 3 (OR)"),
+        custom.coef.names=c("Intercepto","Sexo (Mujer=1)","Edad","Primaria","Secundaria", "Técnica o Superior","Universitaria o postgrado"),
+        ci.force = c(TRUE,TRUE),
+        override.coef = list(coef(m03),or@coef),
+        custom.gof.rows=list("Deviance Test ($p$)" = c(test.pvalues3,
+                                                       test.pvalues3),
+                             "Pseudo R2" = c(mfr2.03,mfr2.03)),
+        custom.note = "$^{***}$ p < 0.001; $^{**}$ p < 0.01; $^{*}$ p < 0.05 <br> Errores estándar entre paréntesis. <br> **Nota**: La significancia estadística de los coeficientes en unidades de Odds ratio está calculada en base a los valores $t$, <br> los cuales a su vez se calculan en base a $log(Odds)/SE$")
+
+# graficamente
+plot02<- plot_model(m03,vline.color = "grey")
+plot01<- plot_model(m03,vline.color = "grey",transform = NULL)
+plot_grid(list(plot02,plot01),tags = c(" "," "),
+          margin = c(0,0,0,0))
--- processing/code-clase-11.R
+++ processing/code-clase-11.R
@@ -1,22 +1,10 @@
-# ---- 0. Identificacion y descripción general ---- 
+# Codigo practica 11: Transformacion de variables y supuestos.
 
----
-# title: "Práctica 11 EMV"
-# author: "Grupo 5 y Grupo 7 EMV 2020"
-# date: "20-08-2020"
----
-  
-## El objetivo de esta practica es revisar un apartado de transformacion de variables y otro analisis 
-## de supuestos y robustez estadistica de regresion. Utilizaremos la base de datos ELSOC 2018 para analizar los determinantes de la participación cuidadana
-  
-# ---- 1. Librerias ----
-
-## Fijamos directorio de trabajo
-## Librerias
-
+# 1. Cargar librerias ----
 pacman::p_load(dplyr, 
                summarytools, 
-               sjPlot,texreg, 
+               sjPlot,
+               texreg, 
                corrplot,
                ggplot2,
                ggfortify,
@@ -24,48 +12,37 @@ pacman::p_load(dplyr,
                lmtest,
                sjlabelled)
 
-# ---- 2. Datos ---- 
-
-# ELSOC cuenta con una muestra final de 3748 casos en el 2018
-
-#Cargamos la base de datos desde internet
+# 2. Cargar datos ---- 
 load(url("https://multivariada.netlify.com/assignment/data/elsoc18p11.RData"))
 
-# ---- 3. Descriptivos y relacion entre variables ----
+# 3. Descriptivos ----
 
 view(dfSummary(elsoc, headings = FALSE, method = "render"))
 
 view_df(elsoc,max.len = 50)
 
-# ---- 4. Medición y transformación de variables ---- 
+# 4. Medicion y transformacion de variables ---- 
 
-# ---- 4.1. Creacion Indice ---- 
+# 4.1. Crear indice PP ---- 
 
-## Para aunar preguntas referentes a la participacon politica, se usaran cuatro donde se les pregunta por su frecuencia de participacion
-## en determinados evenetos. Se emplean escalas likert de 5 categorias para medir dica participacion 
+## 4 preguntas referentes a participacion politica (likert)
 
 plot_stackfrq(elsoc[,c("part01","part02","part03","part04")]) + theme(legend.position="bottom")
 
-## Como sabemos, para crear un indie primero debemos verificar si nuetsros indicadores estan correlacionados
+## Para crear un indice primero debemos verificar si nuestros indicadores estan correlacionados
 
 corrplot.mixed(cor(select(elsoc,part01,part02,part03,part04),
                    use = "complete.obs"))
 
-## La matriz nos muestra correlaciones moderadas entre los indicadores = 0.25 la mas baja y 0.5 la mas alta.
-## Una vez hecho esto se decide crear un indice sumatorio simple. Para ello se usa la funcion mutate y rowSums()
+## Crear un indice sumatorio simple
 
-elsoc <- elsoc %>% mutate(partpol=rowSums(select(., part01,part02,part03,part04))) #con mutate se crea una nueva variable y con rowSums se suman los indicadores 
+elsoc <- elsoc %>% mutate(partpol=rowSums(select(., part01,part02,part03,part04))) 
 
 descr(elsoc$partpol,style = "rmarkdown",stats = "common", transpose = T,headings = F)
 
-plot_frq(data = elsoc$partpol,type = "hist",show.mean = T) # hacemos un plot para observar su distribucion
+plot_frq(data = elsoc$partpol,type = "hist",show.mean = T)
 
-## Entonces: 
-# Los valores del indice van desde 4 hasta 20 
-# La media es 5.47 y la mediana es 4 
-# Esto da cuenta de la proporcion importante que respondio "nunca" [categoria 4] en los indicadores, por ell son represnetados con alta frecuencia en este histograma
-
-# ---- 4.2. Recueprar casos perdidos o Missing's ---- 
+# 4.2. Missing's values ---- 
 
 ## Veamos el caso de los ingresos que es clasico. 
 # ELSOC tiene dos variables para tratar de palear estas complejidades [inghogar] y [inghogar_t] 
@@ -106,11 +83,11 @@ elsoc$inghogar_t[elsoc$inghogar_t==19]<-(1850001+2700000)/2 # [19] "De $1.850.00
 elsoc$inghogar_t[elsoc$inghogar_t==20]<-(2700000)           # [20] "Mas de $2.700.000 a mensuales liquidos"
 
 # -- Paso 2 --
-elsoc$inghogar_i <- ifelse(test = (is.na(elsoc$inghogar)), #¿existen NA en ingresos?
+elsoc$inghogar_i <- ifelse(test = (is.na(elsoc$inghogar)), #Â¿existen NA en ingresos?
                            yes = elsoc$inghogar_t,         #VERDADERO, remplazar con la media del tramo
                            no = elsoc$inghogar)            #FALSE, mantener la variable original.
 
-elsoc$inghogar_i <- set_label(elsoc$inghogar_i,"Ingreso total del hogar (imputada)") # nueva variable
+elsoc$inghogar_i <- set_label(elsoc$inghogar_i,"Ingreso total del hogar (imputada)")
 
 # -- Paso 3 --
 sjmisc::descr(elsoc[,c("inghogar","inghogar_i")],
@@ -120,22 +97,18 @@ sjmisc::descr(elsoc[,c("inghogar","inghogar_i")],
 ## Ahora podemos calcular el ingreso per capita del hogar empleando la variable "cantidad de habitantes hogar"
 
 elsoc$ing_pcap <- elsoc$inghogar_i/elsoc$tamhogar
-elsoc$ing_pcap <- set_label(elsoc$ing_pcap,"Ingreso per cápita del hogar")
+elsoc$ing_pcap <- set_label(elsoc$ing_pcap,"Ingreso per cÃ¡pita del hogar")
 
 sjmisc::descr(elsoc[,c("inghogar","inghogar_i","tamhogar","ing_pcap")],
-              show =c("label", "n", "NA.prc", "mean", "md","sd")) %>% knitr::kable(digits = 2) #descriptivo
+              show =c("label", "n", "NA.prc", "mean", "md","sd")) %>% knitr::kable(digits = 2)
 
-## Finalmente, vemos que la variable [tamhogar] posee un 0.19% de NA's. Por ende
-## la nueva variable [ing_pcap] aumenta un poco sus NA's a un 5.23%
+# 4.3. Ingresos categorica ---- 
 
-
-## ---- 4.3. Ingresos como variable categorica ---- 
-
-# Teniendo el ingreso per capita podemos calcular categorias de ingresos como quintiles o deciles. 
-# Es decir, como una variable categorica. 
+## Teniendo el ingreso per capita podemos calcular categorias de ingresos como quintiles o deciles. 
 
 elsoc$quintile<- dplyr::ntile(x = elsoc$ing_pcap,
                               n = 5) # n de categorias, para quintiles usamos 5 
+
 elsoc$quintile <- factor(elsoc$quintile,c(1,2,3,4,5), c("Quintil 1","Quintil 2","Quintil 3","Quintil 4","Quintil 5")) 
 elsoc %>% 
   group_by(quintile) %>% 
@@ -144,17 +117,16 @@ elsoc %>%
             Mediana=median(ing_pcap,na.rm = T)) %>% 
   knitr::kable()
 
-## Por ultimo, si queremos recuperar ese 5.23% (n=196) de NA's podemos generar una categoria adicional
-## para los datos perdidos, vale decir, recodiricamos los NA's para que se incluyan como nueva categoria
+## Si queremos recuperar ese 5.23% (n=196) de NA's podemos generar una categoria adicional
+## para los datos perdidos, vale decir, recodificamos los NA's para que se incluyan como nueva categoria
 
 elsoc$quintilemiss <- factor(elsoc$quintile,ordered = T)
 elsoc$quintilemiss <- ifelse(is.na(elsoc$quintilemiss),yes = 6,no = elsoc$quintilemiss)
 elsoc$quintilemiss <- factor(elsoc$quintilemiss ,levels = c(1,2,3,4,5,6),labels =  c("Quintil 1","Quintil 2","Quintil 3","Quintil 4","Quintil 5","Missing")) 
 elsoc %>% group_by(quintilemiss) %>% summarise(n=n())
 
-## Así, recuperando con esta nueva categoria podemos usar estos datos para posteriores analisis que veremos a continuacion empleando la variable ingresos
+# 5. Modelos ----
 
-# ---- 5. Estimación ---- 
 fit01<- lm(partpol~sexo+edad+ing_pcap+pospol,data=elsoc)
 fit02<- lm(partpol~sexo+edad+quintile+pospol,data=elsoc)
 fit03<- lm(partpol~sexo+edad+quintilemiss+pospol,data=elsoc)
@@ -162,20 +134,20 @@ fit03<- lm(partpol~sexo+edad+quintilemiss+pospol,data=elsoc)
 labs01 <- c("Intercepto","Sexo (mujer=1)","Edad","Ingreso per/cap","Centro (ref. derecha)","Izquierda","Idep./Ninguno",
             "Quintil 2","Quintil 3","Quintil 4","Quintil 5",
             "Quintil 2","Quintil 3","Quintil 4","Quintil 5","Quintil perdido")
+
 screenreg(list(fit01,fit02,fit03),doctype = FALSE, 
         custom.model.names = c("Modelo 1","Modelo 2","Modelo 3"),
         custom.coef.names = labs01)
 
+# 6. Diagnostico supuestos ---- 
 
-# ---- 6. Diagnosticos ---- 
-
-# ---- 6.1. Casos influyentes ---- 
+# 6.1. Casos influyentes ---- 
 
 ## Para determinar casos influyentes usamos D de Cook. Luego se establece el punto de corte 4/(n-k-1) y se compara
 
 n<- nobs(fit03) #n de observaciones
 k<- length(coef(fit03)) # n de parametros
-dcook<- 4/(n-k-1) #punto de corte
+dcook<- 4/(n-k-1) #punt de corte
 
 # Graficamos 
 final <- broom::augment_columns(fit03,data = elsoc)
@@ -204,21 +176,17 @@ screenreg(list(fit03,fit04),
         custom.model.names = c("Modelo 3", "Modelo 4"),
         custom.coef.names = labs02)
 
-
-## En términos generales, el sentido y significación estadística de los coeficientes del Modelo 4 se mantiene respecto al Modelo 3.
-## Ademas, si observamos que el modelo sin casos influyentes presenta una mejora en ajuste. Por lo tanto, los análisis posteriores se realizaran en base a este modelo
-
-
-# ---- 6.2. Linealidad ---- 
+#  6.2. Linealidad ---- 
 
 ## Analisis de la distribucion de los residuos respecto a la recta de regresion 
-## Los residuos deben ser independientes de los valores predichos y la presencia de un patron no lineal es señal de que el modelo esta espcificando incorrectamente
+## Los residuos deben ser independientes de los valores predichos y la presencia de un patron no lineal es seÃ±al de que el modelo esta especificando incorrectamente
+
 ggplot(fit04, aes(.fitted, .resid)) +
   geom_point() +
   geom_hline(yintercept = 0) +
   geom_smooth(se = TRUE)
 
-## Podemos ver un patron de distribucionde los resdio. Para mejorar la estimacion haremos una transofrmacion de variables. Veamos edad e ingresos
+## Para mejorar la estimacion haremos una transofrmacion de variables: edad e ingresos
 
 ## Polinomio de Edad = Edad^2
 elsoc02$edad2 <- elsoc02$edad^2
@@ -241,52 +209,43 @@ fit06 <- lm(partpol~sexo+edad+edad2+lningreso+pospol,data=elsoc02)
 plot_frq(elsoc02$ing_pcap,type = "hist",normal.curve = T, show.mean = T)
 plot_frq(elsoc02$lningreso,type = "hist", normal.curve = T,show.mean = T)
 
-## Comparamos Modelos 
+## Comparamos modelos 
 
 labs03 <- c("Intercepto","Sexo (mujer=1)","Edad",
             "Quintil 2","Quintil 3","Quintil 4","Quintil 5","Quintil perdido",
-            "Izquierda (ref. derecha)","Centro","Idep./Ninguno", "Edad²","Ingreso per cap (log)")
+            "Izquierda (ref. derecha)","Centro","Idep./Ninguno", "EdadÂ²","Ingreso per cap (log)")
 
 screenreg(list(fit04, fit05, fit06), doctype = FALSE,
         custom.model.names = c("Modelo 4", "Modelo 5", "Modelo 6"), 
         custom.coef.names = labs03)
 
 ## Interpretacion: 
-# El beta de log(ingreso) es de 0.24 lo cual quiere decir que por una unidad de procentaje (1%) que se incrementen los ingresos per 
-# capita del hogar, el promedio del indice de participacion politica auemnta en un 0.24 (o bien un 0.0024) manteniendo todas las variables constantes.
-# EL beta es signifiativo a un 99%. 
+# El beta de log(ingreso)= 0.24 
+# Por una unidad de porcentaje (1%) que se incrementen los ingresos per capita del hogar, el promedio del indice de participacion politica aumenta en 0.24 (o bien un 0.0024) ceteris paribus.
 
-## Comparando modelos 5 (ingresos como quintiles) y 6 (ingresos logaritmo) podemos ver que es mejor quedarse con el 5 dada la cantidad de N 
-
-# ---- 6.3. Homocedasticidad ---- 
+# 6.3. Homocedasticidad ---- 
 car::ncvTest(fit05)
 lmtest::bptest(fit05)
 
-## Tanto el test de Breush-Pagan como el de Cook-Weisberg indican que hay problemas de heterocedasticidad de los resdius
-## puesto que el p-value > 0.05. Por tanto se rechaza la H0 = varianza del error es constante
+## Tanto el test de Breush-Pagan como el de Cook-Weisberg indican que hay problemas de heterocedasticidad de los residuos
 
-## En vista de este problema, debemos calcular los errores robustos para nuestra ultima estimación para corregir los problemas de heterocedasticidad y estimar el ultimo modelo
+## Calcular los errores robustos para corregir los problemas de heterocedasticidad 
 model_robust<- coeftest(fit05, vcov=vcovHC)
 
 # Comparamos modelos 
 
 labs04 <- c("Intercepto","Sexo (mujer=1)","Edad",
             "Quintil 2","Quintil 3","Quintil 4","Quintil 5","Quintil perdido",
-            "Izquierda (ref. derecha)","Centro","Idep./Ninguno", "Edad²")
+            "Izquierda (ref. derecha)","Centro","Idep./Ninguno", "EdadÂ²")
 
 screenreg(list(fit04, fit05, model_robust), doctype = FALSE,
         custom.model.names = c("Modelo 4","Modelo 5", "M5 Robust"), custom.coef.names = labs04)
 
-## Los resultdos del modelo con ES robusto indican que las estimaciones son robustas a la presencia de heterocedasticidad en los reisiduos, 
-## debido a que la signifcancia del beta se mantiene respecto al modelo 5. En suma, la heterocedasticidad no es problematica
+## Con ES robusto indican que las estimaciones son robustas a la presencia de heterocedasticidad en los residuos, 
+## debido a la mantencion de la significancia estadistica. En suma, la heterocedasticidad no es problematica
 
-# ---- 6.4. Multicolinealidad ---- 
+# 6.4. Multicolinealidad ---- 
 car::vif(fit04)
 car::vif(fit05)
 
-## Vemos que en el modelo que no incorpora edad cuadratica no hay probelmas de multicolinealidad. Pero al incorporar la edad cuadratica el VIF es de 6.2, por lo que es problematico ese modelo. 
-
-
-
-
-
+## Vemos que en el modelo que no incorpora edad cuadratica no hay problemas de multicolinealidad, en los que sÃ­ es problematico.
